ba0887c1868eb48faacc44a7bbffc3c1c3bb44e1
Jasmine <jasminewilliams2017@gmail.com>
2020-05-10 22:34:35 -0500

    WIP

--- Controllers/MuscleGroupsController.cs
+++ Controllers/MuscleGroupsController.cs
@@ -42,66 +42,6 @@ namespace GymForMuscles.Controllers
             return muscleGroup;
         }
 
-        // PUT: api/MuscleGroups/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutMuscleGroup(int id, MuscleGroup muscleGroup)
-        {
-            if (id != muscleGroup.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(muscleGroup).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!MuscleGroupExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/MuscleGroups
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPost]
-        public async Task<ActionResult<MuscleGroup>> PostMuscleGroup(MuscleGroup muscleGroup)
-        {
-            _context.MuscleGroup.Add(muscleGroup);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetMuscleGroup", new { id = muscleGroup.Id }, muscleGroup);
-        }
-
-        // DELETE: api/MuscleGroups/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<MuscleGroup>> DeleteMuscleGroup(int id)
-        {
-            var muscleGroup = await _context.MuscleGroup.FindAsync(id);
-            if (muscleGroup == null)
-            {
-                return NotFound();
-            }
-
-            _context.MuscleGroup.Remove(muscleGroup);
-            await _context.SaveChangesAsync();
-
-            return muscleGroup;
-        }
-
         private bool MuscleGroupExists(int id)
         {
             return _context.MuscleGroup.Any(e => e.Id == id);
